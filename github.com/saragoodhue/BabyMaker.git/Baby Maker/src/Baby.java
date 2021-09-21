d3e84922c5ede64d1f42ccacdc2e861d7cf383ba
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2021-09-21 11:27:04 -0600

    made one baby with name and it cries

---
+++ github.com/saragoodhue/BabyMaker.git/Baby Maker/src/Baby.java
@@ -0,0 +1,20 @@
+
+public class Baby
+	{
+
+		String name;
+		double weight;
+		boolean isGirl;
+		
+		public Baby(String n, double w, boolean g)
+		{
+			name = n;
+			weight = w;
+			isGirl = g;
+		}
+
+		public static void cry()
+		{
+			System.out.println("Will someone make the baby stop crying!");
+		}
+	}
---
+++ github.com/saragoodhue/BabyMaker.git/Baby Maker/src/MakeBaby.java
@@ -0,0 +1,11 @@
+
+public class MakeBaby
+	{
+		public static void main(String[] args)
+			{
+				Baby kaitlynsBaby = new Baby("Abby", 7.6, true);
+				System.out.println("Kaitlyn's baby is named " + kaitlynsBaby.name);
+				kaitlynsBaby.cry();
+			}
+
+	}
