ce19e339f046c64255656545947352910c2d0565
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2021-09-22 11:55:37 -0600

    private

--- github.com/saragoodhue/BabyMaker.git/Baby Maker/src/MakeBaby.java
+++ github.com/saragoodhue/BabyMaker.git/Baby Maker/src/MakeBaby.java
@@ -4,7 +4,10 @@ public class MakeBaby
 		public static void main(String[] args)
 			{
 				Baby kaitlynsBaby = new Baby("Abby", 7.6, true);
-				System.out.println("Kaitlyn's baby is named " + kaitlynsBaby.name);
+				System.out.println("Kaitlyn's baby is named " + kaitlynsBaby.getName());
+				kaitlynsBaby.setName("Michael");
+				System.out.println(kaitlynsBaby.getName());
+				
 				kaitlynsBaby.cry();
 			}
 
