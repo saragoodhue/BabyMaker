2c574ead88598b2cc2a949e2a2d92cbf007f50d8
stevengoodhue <stevengoodhue@192.168.0.21>
2021-09-27 18:46:45 -0600

    cannot get gender boolean to come out right

--- github.com/saragoodhue/BabyMaker.git/Baby Maker/src/MakeBaby.java
+++ github.com/saragoodhue/BabyMaker.git/Baby Maker/src/MakeBaby.java
@@ -6,6 +6,19 @@ public class MakeBaby
 				Baby kaitlynsBaby = new Baby("Abby", 7.6, true);
 				System.out.println("Kaitlyn's baby is named " + kaitlynsBaby.name);
 				kaitlynsBaby.cry();
+				
+				Baby firstBaby = new Baby("Connor", 8.2, false);
+				System.out.println("My first baby weighs " + firstBaby.weight);
+				
+				if(firstBaby.isGirl = true)
+					{
+						System.out.println("My first baby is a girl!");
+					}
+				
+				else if(firstBaby.isGirl = false)
+					{
+						System.out.println("My first baby is a boy!");
+					}
 			}
 
 	}
